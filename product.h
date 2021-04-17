029728499ffabce807eef32c1429cfb9a32859d5
jicongjy <dangmujy@gmail.com>
2021-04-18 02:20:20 +0900

    헤더파일수정

--- product.h
+++ product.h
@@ -15,3 +15,6 @@ int selectDataNo(Product *p, int count); // 번호 선택
 int UpdateProduct(Product *p); //제품을 정보를 수정하는 함수
 int deleteProduct(Product *p); //제품을 삭제하는 함수
 int selectMenu(); //메뉴를 선택하는 함수
+void saveData(Product *p, int count);//파일로 저장하는 함수
+int loadData(Product *p);//파일을 로드하는함수
+void searchName(Product *p, int count);//검색하는 함수
